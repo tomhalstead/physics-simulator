3a2d05e93d24dc541482268e258fb3333ad37be8
sagewang <superiorxmas@gmail.com>
2013-10-23 09:43:51 -0700

    upload headfiles

---
+++ include/whatever.h
@@ -0,0 +1,15 @@
+#ifndef WHATEVER_H
+#define WHATEVER_H
+#include "PointMass.h"
+
+class Whatever
+{
+public:
+    Whatever();
+    void WhateverFunctions();
+
+
+    PointMass* list;
+};
+
+#endif // WHATEVER_H
