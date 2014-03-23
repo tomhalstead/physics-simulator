7abdff3150344ace27c066f3cc94980f21c32d1e
Eli Selkin <eselkin@go.pasadena.edu>
2014-03-22 23:34:00 -0700

    TODO CHECKLIST STARTED

--- include/Vectors.h
+++ include/Vectors.h
@@ -7,18 +7,18 @@ class Vector
 {
 public:
     Vector(T* set);
-    Vector(const T& A); // set <size> amount of 'A' in type T to Vector;
+    Vector(const T& A); // Sets <size> amount of 'A' (of type T) into Vector; // Like memset???
     Vector(const Vector<T,size>& A);// Big3
-    ~Vector();// Big3
-    Vector<T,size>& operator = (const Vector<T,size>& RHS); // Big3
+    ~Vector();// Big3, Destructor
+    Vector<T,size>& operator  = (const Vector<T,size>& RHS); //Big3, Assignment Op. []chk==, []del, []assign
     Vector<T,size>& operator += (const Vector<T,size>& RHS);
     Vector<T,size>& operator -= (const Vector<T,size>& RHS);
-    Vector<T,size> operator + (const Vector<T,size>& RHS) const;
-    Vector<T,size> operator - (const Vector<T,size>& RHS) const;
-    T operator * (const Vector<T,size>& RHS) const; // Dot product, return a scalar
-    Vector<T,size> operator * (const T& C) const; // scalar mutiplication
-    Vector<T,size>& operator *=(const T& C); // Scalar mutiplication
-    friend Vector<T,size> operator *(const T& C, const Vector<T,size>& RHS); // scalar mutiplication
+    Vector<T,size>  operator  + (const Vector<T,size>& RHS) const; // Vector Translation
+    Vector<T,size>  operator  - (const Vector<T,size>& RHS) const; // Vector Translation
+    Vector<T,size>  operator  * (const T& C) const; // Scalar mutiplication
+    Vector<T,size>& operator *= (const T& C);       // Scalar mutiplication
+    T operator * (const Vector<T,size>& RHS) const; // Dot product, This returns a scalar
+    friend Vector<T,size> operator *(const T& C, const Vector<T,size>& RHS); // Scalar mutiplication
     T Magnitude(); // return the size of Vector
     Vector<T,size>& operator [](unsigned int index);
     const Vector<T,size>& operator [](unsigned int index) const;
@@ -29,8 +29,8 @@ private:
 template <class T>
 class Vector3D:public Vector<T,3>
 {
-    Vector3D<T> operator ^(const Vector3D<T>& RHS) const; // cross product
-    Vector3D<T>& operator ^=(const Vector3D<T>& RHS);
+    Vector3D<T> operator ^(const Vector3D<T>& RHS) const; // Cross Product // Can we overload the exclusive or?
+    Vector3D<T>& operator ^=(const Vector3D<T>& RHS);     // Cross
 
 };
 
