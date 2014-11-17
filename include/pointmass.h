31831afcb7af8bdfb449f626e189ced6f5013f76
Thomas Halstead <thalstead@go.pasadena.edu>
2014-11-17 00:22:33 -0800

    Removed erroneous additions to pointmass.h
    Removed erroneous template parameter additions to PointMass class which
    were temporarily considered and decided against.

--- include/pointmass.h
+++ include/pointmass.h
@@ -6,7 +6,7 @@
 #include "Vectors.h"
 
 namespace PhysicsEngine {
-    template <typename PointType = double, typename MassType = double, typename TimeType = double>
+    template <typename T = double>
     class PointMass
             // 'PointMass' objects are designed to model objects in the physics simulation. Since they have no geometry (shape)
             // data, they are intended to be inherited from, so that PointMass objects of different shapes (i.e. spheres or cubes)
@@ -15,26 +15,26 @@ namespace PhysicsEngine {
             // Should this class be made abstract, so that only children of this class can be instantiated?
     {
     public:
-        PointMass(MassType newMass = 0,
-                  const Vectors::Vector3D<PointType> &newPosition = Vectors::Vector3D<PointType>(0.),
-                  const Vectors::Vector3D<PointType> &newVelocity = Vectors::Vector3D<PointType>(0.));
-        PointMass(const Vectors::Vector3D<PointType>& newPosition);
+        PointMass(T newMass = 0,
+                  const Vectors::Vector3D<T> &newPosition = Vectors::Vector3D<T>(),
+                  const Vectors::Vector3D<T> &newVelocity = Vectors::Vector3D<T>());
+        PointMass(const Vectors::Vector3D<T>& newPosition);
 
-        PointMass(const PointMass<PointType> &other);
+        PointMass(const PointMass<T> &other);
         virtual ~PointMass();
 
-        void setPosition(const Vectors::Vector3D<PointType> &newPosition);
-        void setVelocity(const Vectors::Vector3D<PointType> &newVelocity);
-        void setMass(MassType newMass);
-        void setNetForce(const Vectors::Vector3D<PointType> &newNetForce);
-        void addForce(const Vectors::Vector3D<PointType> &extraForce);
+        void setPosition(const Vectors::Vector3D<T> &newPosition);
+        void setVelocity(const Vectors::Vector3D<T> &newVelocity);
+        void setMass(T newMass);
+        void setNetForce(const Vectors::Vector3D<T> &newNetForce);
+        void addForce(const Vectors::Vector3D<T> &extraForce);
 
-        const Vectors::Vector3D<PointType>& getPosition() const;
-        const Vectors::Vector3D<PointType>& getVelocity() const;
-        const Vectors::Vector3D<PointType>& getNetForce() const;
-        MassType getMass() const;
+        const Vectors::Vector3D<T>& getPosition() const;
+        const Vectors::Vector3D<T>& getVelocity() const;
+        const Vectors::Vector3D<T>& getNetForce() const;
+        T getMass() const;
 
-        Vectors::Vector3D<PointType> getTrajectory(TimeType time) const;
+        Vectors::Vector3D<T> getTrajectory(T time) const;
         // This function returns a 'Vector3D' representing the new position of the calling 'PointMass' object after
         // 'time' seconds, given a constant force ('netForce') applied over that time period.
         //
@@ -42,15 +42,15 @@ namespace PhysicsEngine {
         // data, but rather is computed every time the function is called. Perhaps should be called 'calculateNewPosition'
         // or 'calculateNextPosition' to more clearly show what is being represented by the returned value.
 
-        void advance(TimeType time);
+        void advance(T time);
         // Updates position and velocity of the point mass to what they should be after having 'netForce' force
         // applied for 'time' seconds.
 
     private:
-        Vectors::Vector3D<PointType> position;
-        Vectors::Vector3D<PointType> velocity;
-        Vectors::Vector3D<PointType> netForce;
-        MassType mass;
+        Vectors::Vector3D<T> position;
+        Vectors::Vector3D<T> velocity;
+        Vectors::Vector3D<T> netForce;
+        T mass;
     };
 }
 #endif // POINTMASS_H
